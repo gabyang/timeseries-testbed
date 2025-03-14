ca646e2e56684f45f9bfccc3cc7a78cad459e5e9
Yueh-Ching <111058821+e1093051@users.noreply.github.com>
2025-03-14 16:15:46 +0800

    Update create_materialized_view.sql

--- create_materialized_view.sql
+++ create_materialized_view.sql
@@ -1,3 +1,4 @@
+-- because optimisation should work on all patterns, and there exist patterns that reference data from the previous 4 days, so materialized view is created with 5-day duration.
 CREATE MATERIALIZED VIEW daily_ohlc_prev AS
 SELECT 
     day,
@@ -15,7 +16,17 @@ SELECT
     LAG(close, 2) OVER (ORDER BY day) AS prev_prev_close, -- Second previous day's close
 	LAG(high, 2) OVER (ORDER BY day) AS prev_prev_high, 
     LAG(low, 2) OVER (ORDER BY day) AS prev_prev_low, 
-	LAG(volume, 2) OVER (ORDER BY day) AS prev_prev_volume 
+	LAG(volume, 2) OVER (ORDER BY day) AS prev_prev_volume,
+    LAG(open, 3) OVER (ORDER BY day) AS prev3_open, 
+    LAG(close, 3) OVER (ORDER BY day) AS prev3_close, 
+	LAG(high, 3) OVER (ORDER BY day) AS prev3_high, 
+    LAG(low, 3) OVER (ORDER BY day) AS prev3_low, 
+	LAG(volume, 3) OVER (ORDER BY day) AS prev3_volume,
+    LAG(open, 4) OVER (ORDER BY day) AS prev4_open, 
+    LAG(close, 4) OVER (ORDER BY day) AS prev4_close, 
+	LAG(high, 4) OVER (ORDER BY day) AS prev4_high, 
+    LAG(low, 4) OVER (ORDER BY day) AS prev4_low, 
+	LAG(volume, 4) OVER (ORDER BY day) AS prev4_volume
 FROM 
-    daily_ohlc_aligned
+    daily_ohlc
 ORDER BY day;
